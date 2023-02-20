c9fffa552e7860ec25d8c53dbda894d565ea213a
mashxurdev <mashxurdev@gmail.com>
2023-02-20 21:14:52 +0500

    users

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import Admin from "./page/admin/Admin";
 import Home from './page/home/h'
 import Login from './Login/Login'
 import Sertifikat from "./page/sertificat/s";
+import Total from "./page/sertificatTotal/Total";
 
 function Main() {
   return (
@@ -21,6 +22,9 @@ function Main() {
         </Route>
         <Route exact path={'/admin'}>
           <Admin/>
+        </Route>
+        <Route exact path={'/users'}>
+          <Total/>
         </Route>
       </Switch>
     </>
